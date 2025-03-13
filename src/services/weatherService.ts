a195b7c6bdc2d3daaed191f2974594328e2dcd36
katinyang22 <u2136681@uel.ac.uk>
2025-03-13 12:11:01 +0000

    added city names to weather service

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -3,6 +3,7 @@ import { faker } from "@faker-js/faker";
 export const generateLondonWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData = {
+    city: "London",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
@@ -16,6 +17,7 @@ export const generateLondonWeatherData = (): WeatherData => {
 export const generateDublinWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
+    city: "Dublin",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
@@ -29,6 +31,7 @@ export const generateDublinWeatherData = (): WeatherData => {
 export const generateParisWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
+    city: "Paris",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
@@ -41,6 +44,7 @@ export const generateParisWeatherData = (): WeatherData => {
 export const generateMadridWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
+    city: "Madrid",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
