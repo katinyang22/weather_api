df19010f5fb9ec251b059782517a2b13b5ea62b3
katinyang22 <u2136681@uel.ac.uk>
2025-03-13 12:10:06 +0000

    added config code

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,17 @@
-// your config code
+import algosdk from "algosdk";
+
+const algodToken = "a".repeat(64);
+const server: string = "http://localhost";
+const port: string = "4001";
+
+const mnemonic: string = "shine process winter later social height bag subject pass winner cloud upgrade wreck nut velvet usage say sugar remove chase spike flower luggage able violin";
+
+export function getClient(): algosdk.Algodv2 { 
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+
+export function getAccount(): algosdk.Account{
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+}
